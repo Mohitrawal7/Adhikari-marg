102c34f9201e5bc423087391baeb8cf78c50cd24
Mohitrawal7 <rawalmohit12@gmail.com>
2025-11-04 19:24:49 +0545

    first setup

---
+++ Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/AdhikariMargBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.Adhikari_marg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhikariMargBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhikariMargBackendApplication.class, args);
+	}
+
+}
---
+++ Adhikari-marg-backend/src/test/java/com/mohit/Adhikari_marg_backend/AdhikariMargBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.Adhikari_marg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhikariMargBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
