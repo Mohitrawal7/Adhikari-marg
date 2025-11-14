a799fbf95816f6286b8602b121e5b0d22b0cebf0
Mohitrawal7 <rawalmohit12@gmail.com>
2025-11-14 22:04:01 +0545

    last try on deploy

--- Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/WebConfig.java
+++ Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://adhikari-marg.vercel.app/")
+                .allowedOrigins("https://adhikari-marg.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
