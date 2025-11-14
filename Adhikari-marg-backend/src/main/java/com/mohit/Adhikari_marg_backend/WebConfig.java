eb14bed1720005704292eee605b431d3cea3ab56
Mohitrawal7 <rawalmohit12@gmail.com>
2025-11-14 21:39:38 +0545

    change in code

--- Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/WebConfig.java
+++ Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173",
-                        "https://adhikari-marg.vercel.app/")
+                .allowedOrigins("https://adhikari-marg.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- Adhikari-marg-frontend/src/api/axiosConfig.js
+++ Adhikari-marg-frontend/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL 
+  // || "http://localhost:8080",
 });
 
 // Add a request interceptor
