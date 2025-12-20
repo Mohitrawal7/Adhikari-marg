6802d16dbc6de9f71fe0d3b4af783714cdbce9ff
Mohitrawal7 <rawalmohit12@gmail.com>
2025-12-20 19:22:10 +0545

    changes -2

--- Adhikari-marg-frontend/src/api/axiosConfig.js
+++ Adhikari-marg-frontend/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://adhikari-marg.onrender.com" || "http://localhost:8080"
+   baseURL: // "https://adhikari-marg.onrender.com" ||
+   "http://localhost:8080"
 });
 
 // Add a request interceptor
