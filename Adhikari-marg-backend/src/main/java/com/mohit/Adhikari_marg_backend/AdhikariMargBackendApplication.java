33e664703fb2888f88ae98c2208e8f91a4f55cc1
Mohitrawal7 <rawalmohit12@gmail.com>
2026-01-30 21:15:52 +0545

    update for port22

--- Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/AdhikariMargBackendApplication.java
+++ Adhikari-marg-backend/src/main/java/com/mohit/Adhikari_marg_backend/AdhikariMargBackendApplication.java
@@ -1,5 +1,6 @@
 package com.mohit.Adhikari_marg_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,5 +12,13 @@ public class AdhikariMargBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdhikariMargBackendApplication.class, args);
 	}
+
+
+	@PostConstruct
+	public void checkEnv() {
+		System.out.println("URL=" + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("USER=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
+	}
+
 
 }
